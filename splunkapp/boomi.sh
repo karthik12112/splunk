e9c2d6db6525a173c1094f1b307dc993b6778cce
Karthik Reddy <karthik.reddy@experianinteractive.com>
2021-03-06 21:07:49 -0800

    final update

--- splunkapp/boomi.sh
+++ splunkapp/boomi.sh
@@ -3,8 +3,19 @@ wget https://new-mounika.s3.amazonaws.com/splunkforwarder-8.1.2-545206cc9f70-lin
 sudo yum install splunkforwarder-8.1.2-545206cc9f70-linux-2.6-x86_64.rpm -y
 vi /opt/splunkforwarder/etc/system/local/inputs.conf
 [monitor:///opt/Boomi_AtomSphere/Atom/Atom_mounikaAWS/execution/history/*/execution-*/process_log.xml]
-host=mounika
-sourcetype=log4net_xml
+host=kk
+sourcetype=karthik
+time_before_close = 15
+multiline_event_extra_waittime = true
+
+vi /opt/splunkforwarder/etc/system/local/user-seed.conf
+[user_info]
+USERNAME = splunkadmin
+PASSWORD = !QSYk68bI^$P2hXV^c2H7s(B
 /opt/splunkforwarder/bin/splunk start --answer-yes --no-prompt --accept-license
-/opt/splunkforwarder/bin/splunk add forward-server 52.27.156.126:9997
+/opt/splunkforwarder/bin/splunk add forward-server 18.237.49.246:9997
 /opt/splunkforwarder/bin/splunk add monitor /var/log/cloud-init-output.log -sourcetype linux_secure
+
+
+
+/opt/splunkforwarder/bin/splunk restart
--- splunkapp/splunk.sh
+++ splunkapp/splunk.sh
@@ -1,3 +1,12 @@
 wget https://new-mounika.s3.amazonaws.com/splunk-8.1.2-545206cc9f70-linux-2.6-x86_64.rpm
 sudo yum install splunk-8.1.2-545206cc9f70-linux-2.6-x86_64.rpm -y
 /opt/splunk/bin/splunk start --accept-license --answer-yes --no-prompt --seed-passwd Administrator
+
+
+
+/opt/splunk/bin/splunk restart
+vi /opt/splunk/etc/apps/search/local/props.conf
+
+[karthik]
+BREAK_ONLY_BEFORE = <ExecutionLogModel>
+MAX_EVENTS = 10000
